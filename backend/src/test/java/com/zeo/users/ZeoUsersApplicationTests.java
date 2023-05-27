251c16a07a51d0a749cd49a0ca39c149165c932c
Yongliang Zhan <yzhan94@gmail.com>
2023-05-27 11:20:34 +0200

    Initial commit
    Created backend and frontend project seeds with
    Spring Initializr and Angular CLI

---
+++ backend/src/test/java/com/zeo/users/ZeoUsersApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zeo.users;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeoUsersApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
